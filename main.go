a14d2529b479cbb9ae205306c1ade40783663c99
Oleh Lamzin <olehtomas@gmail.com>
2022-03-11 23:39:32 +0100

    Generate 1-999 pallets

--- main.go
+++ main.go
@@ -3,42 +3,40 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/png"
 	"os"
 	"os/exec"
 
 	"github.com/boombuler/barcode"
 	"github.com/boombuler/barcode/code128"
 	"github.com/fogleman/gg"
+	"github.com/nfnt/resize"
 )
 
 func main() {
-	// if err := os.Mkdir("out", 0666); err != nil {
-	// 	panic(err)
-	// }
+	const pdfFileName = "out/Pallets.pdf"
+
+	// Remove old file.
+	os.Remove(pdfFileName)
 
 	var files []string
-	for i := 1; i < 10; i++ {
+	for i := 1; i < 1000; i++ {
 		text := fmt.Sprintf("Pallet-%d", i)
 		DrawPNG(text)
 
 		fileName := fmt.Sprintf("out/%s.png", text)
+		defer os.Remove(fileName)
+
 		files = append(files, fileName)
 	}
 
 	args := []string{"import", "out/Pallets.pdf"}
 	args = append(args, files...)
-	fmt.Println(args)
 
 	cmd := exec.Command("pdfcpu", args...)
 	err := cmd.Run()
 	if err != nil {
 		panic(err)
-	}
-
-	for _, fileName := range files {
-		if err := os.Remove(fileName); err != nil {
-			panic(err)
-		}
 	}
 }
 
@@ -50,12 +48,18 @@ func DrawPNG(text string) {
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 
+	dc.DrawImage(ReadHeart(), 10, 10)
+	dc.DrawImage(ReadHeart(), W-250-10, 10)
+	dc.DrawImage(ReadHeart(), 10, H-250-10)
+	dc.DrawImage(ReadHeart(), W-250-10, H-250-10)
+
 	dc.SetRGB(0, 0, 0)
 	if err := dc.LoadFontFace("/Library/Fonts/Arial Unicode.ttf", P); err != nil {
 		panic(err)
 	}
 	dc.DrawStringWrapped(text, W/2, P, 0.5, 0, 0, 1.5, gg.AlignCenter)
 	dc.DrawImage(GenerageBarCode(text), 300, 400)
+
 	if err := dc.SavePNG(fmt.Sprintf("out/%s.png", text)); err != nil {
 		panic(err)
 	}
@@ -65,4 +69,18 @@ func GenerageBarCode(text string) image.Image {
 	barCode, _ := code128.Encode(text)
 	scaledBarCode, _ := barcode.Scale(barCode.(barcode.Barcode), 1000, 300)
 	return scaledBarCode
+}
+
+func ReadHeart() image.Image {
+	file, err := os.Open("data/pallet.png")
+	if err != nil {
+		panic(err)
+	}
+	img, err := png.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	scaled := resize.Resize(250, 0, img, resize.Lanczos3)
+	return scaled
 }
